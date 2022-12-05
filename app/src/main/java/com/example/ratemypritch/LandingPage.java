052e266982e72d04f16299c3ec11d7a9b5aec10f
Jayati <jae186@gmail.com>
2022-12-05 14:42:11 -0800

    covid api working'

--- app/src/main/java/com/example/ratemypritch/LandingPage.java
+++ app/src/main/java/com/example/ratemypritch/LandingPage.java
@@ -2,6 +2,7 @@ package com.example.ratemypritch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -15,6 +16,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -27,7 +29,7 @@ import okhttp3.Response;
 public class LandingPage extends AppCompatActivity {
 
  //   DB_Helper DB;
-    TextView txtString;
+    TextView txtString, txtString2;
     public String url= "https://quotes-inspirational-quotes-motivational-quotes.p.rapidapi.com/quote?token=ipworld.info";
 
     @Override
@@ -40,9 +42,11 @@ public class LandingPage extends AppCompatActivity {
         setContentView(R.layout.activity_landing_page);
 
         txtString= (TextView)findViewById(R.id.txtRequest);
+        txtString2= (TextView)findViewById(R.id.textView16);
 
         try {
             run();
+            run2();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -62,6 +66,7 @@ public class LandingPage extends AppCompatActivity {
         loglog.setOnClickListener(new View.OnClickListener() {
             Boolean loggedin = false;
 
+            @SuppressLint("SuspiciousIndentation")
             @Override
             public void onClick(View view) {
 //                loggedin = true;
@@ -106,6 +111,8 @@ public class LandingPage extends AppCompatActivity {
 
         OkHttpClient client = new OkHttpClient();
 
+
+
         Request request = new Request.Builder()
                 .url(url)
                 .get()
@@ -129,6 +136,44 @@ public class LandingPage extends AppCompatActivity {
                     public void run() {
                         String[] bro = myResponse.split(":");
                         txtString.setText(bro[2].substring(0,bro[2].length()-1));
+                    }
+                });
+
+            }
+        });
+    }
+
+    void run2() throws IOException {
+
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://covid-193.p.rapidapi.com/statistics?country=Canada")
+                .get()
+                .addHeader("X-RapidAPI-Key", "fb352cc763msh83751fccbd4f286p102897jsn2591a4b47bf5")
+                .addHeader("X-RapidAPI-Host", "covid-193.p.rapidapi.com")
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                call.cancel();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+
+                final String myResponse = response.body().string();
+
+                LandingPage.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        String[] bros = myResponse.split(":");
+                        String[] apple = bros[13].split(",");
+                        int cases = Integer.parseInt(apple[0]);
+                        if(cases<100)
+                            txtString2.setText("It's safe to go to Pritchard!");
+                        else
+                            txtString2.setText("Be careful and mask up!");
                     }
                 });
 
